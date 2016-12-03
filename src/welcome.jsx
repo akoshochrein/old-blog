7655cdb15637916b4e9a8d7e29e9ac263db30b61
akoskaaa <akos.hochrein@prezi.com>
2016-12-03 17:03:40 +0100

    massage welcome page

--- src/welcome.jsx
+++ src/welcome.jsx
@@ -9,7 +9,8 @@ export default class Welcome extends React.Component {
     render () {
         return (
             <div className="application-wrapper">
-                <h1 className="welcome">Welcome</h1>
+                <h1 className="welcome">akos hochrein</h1>
+                <h2 className="welcome">software engineer</h2>
                 <div className="welcome-menu">
                     <MenuItem linkTo="showcase" name="Showcase" backgroundImage={tree} />
                     <MenuItem linkTo="blog/test" name="Blog" backgroundImage={orange} />
