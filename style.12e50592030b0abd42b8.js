e87cba8c6bb31410a7aaacb07fe9c215efbb2462
akoskaaa <akos.hochrein@prezi.com>
2016-12-03 12:47:27 +0100

    Updates

--- style.12e50592030b0abd42b8.js
+++ style.12e50592030b0abd42b8.js
@@ -1,2 +1,2 @@
 webpackJsonp([1,3],{0:function(n,o,c){n.exports=c(224)},224:function(n,o){}});
-//# sourceMappingURL=style.af3f180b87ae378eeb49.js.map
+//# sourceMappingURL=style.12e50592030b0abd42b8.js.map
